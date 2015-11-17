1b21610fd84ce5c199dece1aa5041c8d66a1a3dd
mesargent <markesargent@gmail.com>
2015-11-17 00:01:22 -0800

    Removed useless menu from results view.

--- app/src/main/java/com/detroitteatime/datagatherer/ResultsView.java
+++ app/src/main/java/com/detroitteatime/datagatherer/ResultsView.java
@@ -1,11 +1,7 @@
 package com.detroitteatime.datagatherer;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.datagatherer.R;
@@ -38,39 +34,6 @@ public class ResultsView extends ActionBarActivity {
         super.onDestroy();
 
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle presses on the action bar items
-        switch (item.getItemId()) {
-            case R.id.save_csv:
-                DataAccess.saveToCSVFile(this);
-                return true;
-            case R.id.delete_csv:
-                DataAccess.deleteCSV();
-                return true;
-            case R.id.clear_db:
-                DataBaseHelper dbHelper = DataBaseHelper.getInstance(this);
-                dbHelper = DataBaseHelper.getInstance(this);
-                dbHelper.getWritableDatabase().execSQL("delete from " + DataBaseHelper.SENSOR_TABLE_NAME);
-
-                return true;
-            case R.id.change_predictor:
-                Intent intent = new Intent(ResultsView.this, ModelList.class);
-                startActivity(intent);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 
 
